9974d7a5fe3fdcdd688f853f52e68e36e2ec8735
hyun020215 <hyun020215@sasa.hs.kr>
2021-06-06 03:36:21 +0900

    Initialize App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import MyPage from "./components/MyPage";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <NavBar />
+            <Switch>
+                <Route exact path="/">
+                    <h1>Main</h1>
+                </Route>
+                <Route exact path="/MyPage">
+                    <MyPage />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
