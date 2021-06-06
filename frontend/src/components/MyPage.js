9a4a1e8c1fecc4efa691cb66d30afc4dff1c97d2
hyun020215 <hyun020215@sasa.hs.kr>
2021-06-06 15:55:41 +0900

    Make 'MyPage.js' & 'MyPage.css'

---
+++ frontend/src/components/MyPage.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import PostItem from "./PostItem"
+import "./MyPage.css";
+
+const Page = () => {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        // 목록 조회 요청 전송
+        axios.get(`/api/myposts/${account.ID}`)
+            // 응답이 돌아오면 응답 내용으로 목록을 변경
+            .then(response => {
+                setItems(response.data);
+            });
+    }, []);
+
+    const onDeleteClick = (item) => {
+        axios.delete(`/api/myposts/${item._id}/delete`)
+            .then(() => axios.get(`/api/myposts/${account.ID}`))
+            .then(response => {
+                setItems(response.data)
+            });
+    };
+
+    const postItemEls = items.map(v => (
+        <PostItem
+            key={v._id}
+            title={v.title}
+            date={v.date}
+            author={v.author}
+            content={v.content}
+            onDeleteClick={() => onDeleteClick(v)}
+        />
+    ));
+
+    return (
+        <div className="MyPage">
+            {postItemEls}
+        </div>
+    );
+}
+
+export default Page;
